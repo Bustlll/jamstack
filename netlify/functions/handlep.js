8c68f21d811f8309efa47adfa5942125b03c4f24
BustIII <pgr545434@gmail.com>
2022-05-25 04:26:18 +0200

    handlep

---
+++ netlify/functions/handlep.js
@@ -0,0 +1,30 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+
+exports.handler = async ({ body, headers }) => {
+  try {
+    // check the webhook to make sure it’s valid
+    const stripeEvent = stripe.webhooks.constructEvent(
+      body,
+      headers['stripe-signature'],
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+
+    // only do stuff if this is a successful Stripe Checkout purchase
+    if (stripeEvent.type === 'checkout.session.completed') {
+   console.log("did it")
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ received: true }),
+    };
+  } catch (err) {
+    console.log(`Stripe webhook failed with ${err}`);
+
+    return {
+      statusCode: 400,
+      body: `Webhook Error: ${err.message}`,
+    };
+  }
+};
