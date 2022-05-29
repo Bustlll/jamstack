b8b1021afbfe2a871c1f93c8a56da13e72faaeb7
BustIII <pgr545434@gmail.com>
2022-05-29 15:07:49 +0200

    bx22

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,8 +1,22 @@
 exports.handler = async (event, context) => {
     const { name = "Anonymous" } = event.queryStringParameters;
+    const { instagram = "Anonymous" } = event.queryStringParameters;
+    const { youtube = "Anonymous" } = event.queryStringParameters;
+    const { twitch = "Anonymous" } = event.queryStringParameters;
+    const { reddit = "Anonymous" } = event.queryStringParameters;
+    const { twitter = "Anonymous" } = event.queryStringParameters;
+    const { region = "Anonymous" } = event.queryStringParameters;
+
     return {
       statusCode: 200,
       body: `
-      Hello, ${name}`
+      Hello, ${name},
+      yt, ${youtube},
+      tw, ${twitch},
+      ig, ${instagram},
+      red, ${reddit},
+      tw, ${twitter},
+      reg, ${region}`
+
     };
   };
