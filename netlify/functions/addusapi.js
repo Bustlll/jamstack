327116ca04f5f54c217a4f0e52a876a30f834579
BustIII <pgr545434@gmail.com>
2022-05-31 09:14:40 +0200

    f6

--- netlify/functions/addusapi.js
+++
@@ -1,8 +0,0 @@
-
-
-
-exports.handler = async (event, context) => {
-
-    return { statusCode: 200, body: process.env.SB_UPDATE_CASH };
-  
-};
--- netlify/functions/handlep.js
+++ netlify/functions/handlep.js
@@ -19,47 +19,10 @@ exports.handler = async ({event, body, headers }) => {
     // only do stuff if this is a successful Stripe Checkout purchase
     if (stripeEvent.type === 'checkout.session.completed') {
       
-   
-      const {
-        SUPABASE_URL,
-        SUPABASE_KEY
-      } = process.env;
-      
-   
-      const { createClient } = require('@supabase/supabase-js');
-      const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-
-      const { name = "Anonymous" } = event.queryStringParameters;
-      const { instagram = "Anonymous" } = event.queryStringParameters;
-      const { youtube = "Anonymous" } = event.queryStringParameters;
-      const { twitch = "Anonymous" } = event.queryStringParameters;
-      const { reddit = "Anonymous" } = event.queryStringParameters;
-      const { twitter = "Anonymous" } = event.queryStringParameters;
-      const { region = "Anonymous" } = event.queryStringParameters;
-      const { qt = "Anonymous" } = event.queryStringParameters;
-      
-      
-        function newTime(){
-          let a = new Date();
-          return a.getTime();
-        }
-
-          const { data, error } = await supabase
-              .from('users')
-              .insert([
-               {
-                  cash:  `${qt}`,
-                  name: `${name}`, 
-                  instagram:  `${instagram}`, 
-                  youtube:  `${youtube}`, 
-                  twitch: `${twitch}`, 
-                  reddit: `${reddit}`, 
-                  twitter:  `${twitter}`, 
-                  region: `${region}`, 
-                  Date: newTime(),
-               }
-              ]);
-      
+  <form>
+    <input placeholder="instagram"></input>
+    <input type="submit" />
+  </form>
     }
 
     return {
