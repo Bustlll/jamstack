8b0dc7d72635cb5856371fabeef915ac96c8dff3
BustIII <pgr545434@gmail.com>
2022-05-31 08:03:09 +0200

    g18

---
+++ netlify/functions/myman.js
@@ -0,0 +1,12 @@
+exports.handler = async (event, context) => {
+    const { createClient } = require('@supabase/supabase-js');
+    const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+
+    const { data, error } = await supabase
+    .from('users')
+    .select()
+
+  
+  
+    return { statusCode: 200, body: JSON.stringify(data) };
+}
