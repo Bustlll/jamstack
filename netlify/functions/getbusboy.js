24d0d7598d23fe4de3ee4bad26a9c8f96c66790c
BustIII <pgr545434@gmail.com>
2022-06-03 18:48:22 +0200

    busboy3

--- netlify/functions/getbusboy.js
+++ netlify/functions/getbusboy.js
@@ -49,5 +49,5 @@ module.exports.handler = async (event, context) => {
     const fields = await parseMultipartForm(event)
   
     fields.favourite_food // "sushi"  
-    fields.company_logo   // { filename: "some_logo.svg", type: "image/svg+xml", content: Buffer([...]) }  
+    fields.preorder_for   // { filename: "some_logo.svg", type: "image/svg+xml", content: Buffer([...]) }  
   }
