035d157fc81e663856ab43c7c5c377b18e9ebfd7
BustIII <pgr545434@gmail.com>
2022-06-03 17:02:39 +0200

    hp17

--- netlify/functions/handlep.js
+++ netlify/functions/handlep.js
@@ -30,10 +30,42 @@ exports.handler = async ({event, body, headers }) => {
       //         body: JSON.stringify(data)}})
 
         
-
-        const url = 'https://toxtat.com/wreckthisbeach/adsup';
-        // const result = fetch(`${url}`, { method: 'get' })
-        fetch(`${url}`, {method: 'GET'}).then(response => {  console.log(res);})   
+      const { name = "Anonymous" } = event.queryStringParameters;
+      const { instagram = "Anonymous" } = event.queryStringParameters;
+      const { youtube = "Anonymous" } = event.queryStringParameters;
+      const { twitch = "Anonymous" } = event.queryStringParameters;
+      const { reddit = "Anonymous" } = event.queryStringParameters;
+      const { twitter = "Anonymous" } = event.queryStringParameters;
+      const { region = "Anonymous" } = event.queryStringParameters;
+      const { qt = "Anonymous" } = event.queryStringParameters;
+      
+      
+        function newTime(){
+          let a = new Date();
+          return a.getTime();
+        }
+      
+      
+      
+          const { data, error } = await supabase
+              .from('users')
+              .insert([
+               {
+                  cash:  `${qt}`,
+                  name: `${name}`, 
+                  instagram:  `${instagram}`, 
+                  youtube:  `${youtube}`, 
+                  twitch: `${twitch}`, 
+                  reddit: `${reddit}`, 
+                  twitter:  `${twitter}`, 
+                  region: `${region}`, 
+                  Date: newTime(),
+               }
+              ]);
+        
+        // Did it work?
+        console.log(data);
+      
 
     
 
