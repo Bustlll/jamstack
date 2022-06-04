2bd84cf1ad8565c313a7bef55316ad7267ef266d
BustIII <pgr545434@gmail.com>
2022-06-04 16:33:20 +0200

    t5

--- netlify/functions/checkout2.js
+++ netlify/functions/checkout2.js
@@ -34,7 +34,7 @@ function newTime(){
         price_data: {
           currency: "usd",
           product_data: {
-            name: "Give "+`${name}`+ ", " +`${qt}` + ", Bucks",
+            name: "Give "+`${name}` +`${qt}` + " Bucks",
           },
           unit_amount: quanti,
         },
@@ -47,11 +47,128 @@ function newTime(){
     success_url: "https://toxtat.com",
     cancel_url: "https://toxtat.com/menu.html", 
   })
-  return {
-    statusCode: 303,
-    headers: {
-      Location: session.url
-      
+  try {
+    const event = stripe.webhooks.constructEvent(
+      body,
+      headers['stripe-signature'],
+      process.env.STRIPE_WEBHOOK_SECRET,
+    );
+
+    if (event.type !== 'checkout.session.completed') {
+      return;
     }
+const cash = items[0].amount_subtotal/100;
+ const { data: genre_data, error: genre_error } = await supabase
+ .from('users')
+ .select()
+ .eq("name", session.metadata.name)
+    const genre_id = genre_data[0].cash;
+   
+
+    let summed = Number(cash) + Number(genre_id);
+
+    const { data: book_data, error: book_error } = await supabase
+         .from('users')
+      .update({ cash: summed})
+      .eq('name', session.metadata.name)
+
+      return {
+        statusCode: 303,
+        headers: {
+          Location: session.url
+          
+        },
+        body: JSON.stringify({ received: true }),
+      };
+    } catch (error) {
+      console.error(error);
+      return {
+        statusCode: 400,
+        body: `WebHook error: ${error.message}`,
+      };
+    }
+  
   }
-}
+//   return {
+//     statusCode: 303,
+//     headers: {
+//       Location: session.url
+      
+//     }
+//   }
+// }
+
+
+//func handlef
+
+  // try {
+  //   const event = stripe.webhooks.constructEvent(
+  //     body,
+  //     headers['stripe-signature'],
+  //     process.env.STRIPE_WEBHOOK_SECRET,
+  //   );
+
+  //   if (event.type !== 'checkout.session.completed') {
+  //     return;
+  //   }
+  //   function newTime(){
+  //     let a = new Date();
+  //     return a.getTime();
+  //   }
+  //   const session = event.data.object;
+
+  //   const lineItems = await stripe.checkout.sessions.listLineItems(session.id);
+
+  //   const items = lineItems.data;
+
+
+  //   const { data, error } = await supabase
+  //       .from('users')
+  //       .insert([
+  //        {
+  //           cash:  session.metadata.cash, 
+  //           name: session.metadata.name, 
+  //           instagram:  session.metadata.instagram, 
+  //           youtube:  session.metadata.youtube, 
+  //           twitch: session.metadata.twitch, 
+  //           reddit: session.metadata.reddit, 
+  //           twitter:  session.metadata.twitter, 
+  //           region: session.metadata.region, 
+  //           Date: newTime(),
+  //        }
+  //       ]);
+    //     console.log(data);
+
+
+
+   
+// function supabase, selects name and updates cash with sumed one from stripe + supa before
+
+
+// const cash = items[0].amount_subtotal/100;
+//  const { data: genre_data, error: genre_error } = await supabase
+//  .from('users')
+//  .select()
+//  .eq("name", session.metadata.name)
+//     const genre_id = genre_data[0].cash;
+   
+
+//     let summed = Number(cash) + Number(genre_id);
+
+//     const { data: book_data, error: book_error } = await supabase
+//          .from('users')
+//       .update({ cash: summed})
+//       .eq('name', session.metadata.name)
+
+
+  //   return {
+  //     statusCode: 200,
+  //     body: JSON.stringify({ received: true }),
+  //   };
+  // } catch (error) {
+  //   console.error(error);
+  //   return {
+  //     statusCode: 400,
+  //     body: `WebHook error: ${error.message}`,
+  //   };
+  // }
